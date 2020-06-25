0307a7ce8001cff423553435981896f47baa2541
yuvaanparwaney <yuvaanparwaney@gmail.com>
2020-06-24 20:53:38 -0700

    test

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1img);
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2img);
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3img);
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,14 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("#c68767");
+      //imageMode(CENTER)
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +85,14 @@ class Game {
       player.update();
     }
 
+    if(player.distance>3860){
+      gameState = 2;
+    }
+
     drawSprites();
+  }
+  end(){
+    text("Game Over",displayWidth/2 - 70, displayHeight/4);
+    game.update(2);
   }
 }
