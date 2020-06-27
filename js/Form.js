252f37da97441b32cb80e19ded74a5ab5a21a037
yuvaanparwaney <yuvaanparwaney@gmail.com>
2020-06-26 17:25:31 -0700

    test

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton('Reset');
   }
   hide(){
     this.greeting.hide();
@@ -19,6 +20,7 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.reset.position(displayWidth-100,20);
 2
     this.button.mousePressed(()=>{
       this.input.hide();
@@ -30,6 +32,10 @@ class Form {
       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
     });
 
   }
--- js/Game.js
+++ js/Game.js
@@ -69,9 +69,12 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+          stroke(10);
+          fill('red');
+          ellipse(x,y,60,60)
           camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y
+          camera.position.y = cars[index-1].y;
+
         }
        
         //textSize(15);
